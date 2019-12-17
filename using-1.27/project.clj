5b7631ecfaa25030778bccca7448dc4eed31cf35
david <davidvoler@gmail.com>
2019-12-17 22:39:13 +0200

    interesting errors

--- using-1.27/project.clj
+++ using-1.27/project.clj
@@ -4,6 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"][cnuernber/libpython-clj "1.27"] [alembic "0.3.2"]]
-  :main ^:skip-aot using-1.27
+  :main ^:skip-aot basic-tests.keras-test
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- using-1.27/src/basic_tests/keras_test.clj
+++ using-1.27/src/basic_tests/keras_test.clj
@@ -1,4 +1,4 @@
-(ns using-1.27
+(ns basic-tests.keras-test
   
   
   (:require [libpython-clj.require :as req :refer [require-python]
@@ -38,6 +38,7 @@
 (py/import-as matplotlib.pyplot plt)
 (py/import-as keras.datasets.fashion_mnist fashion_mnist)
 ; (py/import-from keras.datasets.fashion_mnist load_data)
+(py/import-as keras.layers keras-layers)
 
 (py/$. tf __version__)
 (py/$. keras __version__)
@@ -45,7 +46,7 @@
 
 ; (datasets = (py/$. keras datasets))
 (def fashion_mnist_data (py/call-attr fashion_mnist "load_data"))
-
+fashion_mnist_data
 (defmacro def+
   "binding => binding-form
   internalizes binding-forms as if by def.
@@ -78,8 +79,8 @@ train-labels
 (len test-labels)
 
 
-(def mfig (py/import-module "matplotlib.figure"))
-(def plt (py/import-module "matplotlib.pyplot"))
+; (def mfig (py/import-module "matplotlib.figure"))
+; (def plt (py/import-module "matplotlib.pyplot"))
 ; (def fig (py/call-attr plt "figure"))
 ;(py/call-attr plt "figure")
 ; (py/call-attr plt "imshow" (train_images [0]))
@@ -87,8 +88,36 @@ train-labels
 ;; => Syntax error compiling at (27.clj:39:1).
 ;;    Unable to resolve symbol: keras in this context
 
+;; the following code fails - consider using pantera 
+; (train-images (/ train-images 255.0))
+; (test-images (/ test-images 255.0))
+
+(def flatten (py/call-attr-kw keras-layers "Flatten" [] {:input_shape   [28,28]}))
+(def dense (py/call-attr-kw keras-layers "Dense" [128] {:activation  "relu"}))
+(def last-dense (py/call-attr-kw keras-layers "Dense" [10] {:activation "softmax"}))
 
 
+(defonce model (py/call-attr  keras "Sequential" [ (py/call-attr-kw keras-layers "Flatten" [] {:input_shape   [28,28]}) ]))
+;TypeError: The added layer must be an instance of class Layer. Found: <keras.layers.core.Flatten object at 0x667080590>
+
+(defonce model (py/call-attr  keras "Sequential" [ (py/call-attr-kw keras-layers "Layer" [] {:input_shape   [28,28]}) ]))
+;TypeError: The added layer must be an instance of class Layer. Found: <keras.engine.base_layer.Layer object at 0x662ac1890>
+
+
+; (py/call-attr model "add"   flatten )
+
+; (py/call-attr model "add"  (py/call-attr-kw keras-layers "Dense" [128] {:activation  "relu"}) )
+; (py/call-attr model "add"  (py/call-attr-kw keras-layers "Dense" [10] {:activation "softmax"}) )
+; (def model (py/call-attr  keras "Sequential" [(py/call-attr-kw keras-layers "Flatten" [] {:input_shape   [28,28]})
+;                                               (py/call-attr-kw keras-layers "Dense" [128] {:activation  "relu"})
+;                                               (py/call-attr-kw keras-layers "Dense" [10] {:activation "softmax"})]))
+
+
+(py/call-attr-kw  model "compile" [] {:optimizer "adam"
+                                      :loss "sparse_categorical_crossentropy"
+                                      :metrics (py/->py-list ["accuracy"])})
+;; => Syntax error compiling at (27.clj:91:1).
+;;    Unable to resolve symbol: train-images in this context
 
 ;"
 ;; => Syntax error compiling at (27.clj:35:1).
