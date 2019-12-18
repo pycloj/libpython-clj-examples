53644d71a0a7a605d6dd8f1ec85e61614973badb
daslu <daniel.slutsky@gmail.com>
2019-12-18 16:43:13 +0200

    updated dependencies

--- using-1.28/project.clj
+++ using-1.28/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"] [cnuernber/libpython-clj "1.28"] [alembic "0.3.2"]]
-  :main ^:skip-aot basic-tests.text-classification-with-hub
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cnuernber/libpython-clj "1.28"]
+                 [alembic "0.3.2"]
+                 [scicloj/notespace "0.1.0-SNAPSHOT"]]
+  :target-path "target/%s")
